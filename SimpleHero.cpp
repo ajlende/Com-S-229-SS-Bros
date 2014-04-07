d38321b69f3acc63d089f1026f195732915a8c3c
ajlende <ajlende@iastate.edu>
2014-04-07 15:36:05 -0500

    dddds

--- SimpleHero.cpp
+++ SimpleHero.cpp
@@ -69,11 +69,11 @@ void SimpleHero::findPath(GraphMap* map, int start, int end, std::vector<int>* V
 			n = previous[n];
 		}
 
-		printf("Path from %d to %d: ", start, end);
+		waddstr(stdscr, "Path from %d to %d: ", start, end);
 		for (auto& c : *V) {
-    		printf("%d ", c);
+    		waddstr(stdscr, "%d ", c);
 		}
-		printf("\n");
+		waddstr(stdscr, "\n");
 	}
 
 	delete[] visited;
