cd7e36eeb3197064c999ca8a6974652ad330d994
ajlende <ajlende@iastate.edu>
2014-04-09 11:16:48 -0500

    ...

--- SimpleHero.cpp
+++ SimpleHero.cpp
@@ -116,7 +116,7 @@ int SimpleHero::selectNeighbor( GraphMap* map, int x, int y ) {
 		// Check to see if there is a path to all other ACTOR_EATABLEs before setting closest
 		for (int& second : *eatables) {
 			if (e == second) continue;
-			if (this->findPath(map, e, second, second_path)) a_trap = true;
+			if (!this->findPath(map, e, second, second_path)) a_trap = true;
 			second_path->clear();
 		}
 
