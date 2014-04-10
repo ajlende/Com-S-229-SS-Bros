4fef256138de3057b85c835a011c9ef42d8477a6
ajlende <ajlende@iastate.edu>
2014-04-09 20:18:18 -0500

    trying my own inputs again

--- ssbros.cpp
+++ ssbros.cpp
@@ -19,13 +19,13 @@ int main( int argc, char** argv ) {
 
 	bool t = false;
 	bool s = false;
-	char* seed = 0;
-
+	int seed = 0;
+	
+	/* -- Using getopt didn't seem to work --
 	while(true) {
 		static struct option long_options[] = {
 			{"timer",         no_argument,       0, 't'},
-			{"seed",          optional_argument, 0, 's'}
-			/* ,
+			{"seed",          optional_argument, 0, 's'},
 			{"moves",         required_argument, 0, 0},
 			{"hero",          required_argument, 0, 0},
 			{"enemy",         required_argument, 0, 0},
@@ -36,7 +36,7 @@ int main( int argc, char** argv ) {
 			{"delay-eatable", required_argument, 0, 0},
 			{"delay-powerup", required_argument, 0, 0},
 			{"delay",         required_argument, 0, 0},
-			{"render-off",    no_argument,       0, 0} */
+			{"render-off",    no_argument,       0, 0}
 		};
 
 		int option_index = 0;
@@ -54,12 +54,28 @@ int main( int argc, char** argv ) {
 				seed = optarg;
 				break;
 		}
+	} */
+
+	for (int i = 0; i < argc; i++) {
+		if (strcmp(argv[i], "--timer") == 0 || strcmp(argv[i], "-t") == 0) {
+			t = true;
+		}
+		if (strcmp (argv[i], "--seed") == 0 || strcmp(argv[i], "-s") == 0) {
+			s = true;
+			if (sscanf(argv[i + 1], "%d", &seed) == 1) {
+				srand(seed);
+			} else {
+				printf("seed: %ld\n", time(NULL));
+			}
+		}
 	}
 
+
+	/* Use this if statement for getopt
 	if (s) {
 		if (seed) srand(atoi(seed));
 		else printf("seed: %ld\n", time(NULL));
-	}
+	} */
 
 	Actor** actors = (Actor**) malloc(10 * sizeof(Actor*));
 
