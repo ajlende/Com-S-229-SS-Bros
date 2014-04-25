2cc7cda0a197daa3ed2dc1b619b26edd4a7d60f6
Alex Lende <ajlende@iastate.edu>
2014-04-25 16:29:04 -0500

    Lines

--- ssbros.cpp
+++ ssbros.cpp
@@ -29,7 +29,36 @@ int main( int argc, char** argv ) {
 		if (strcmp(argv[i], "--timer") == 0 || strcmp(argv[i], "-t") == 0) {
 			t = true;
 		} else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
-			printf("Options\n\t--moves # – How many turns to allow before quitting.\n\t--hero [id] – Use the actor with the given id for the heroes.\n\t--enemy [id] – Use the actor with the given id for the enemies.\n\t--eatable [id] – Use the actor with the given id for the eatables.\n\t--powerup [id] – Use the actor with the given id for the powerups.\n\t--delay-[type] # – Let actors of the given type only make moves every # turns.\n\t--delay # – Make every move last # miliseconds.\n\t--render-off – Do not render the map, or make timed delays. Useful if you want to use printf for debugging.\n\t--window # gives a hero # moves to eat enemies after eating a powerup.\n\nAvailable id's\n\tsimplehero - My Simple Hero.\n\tsmarthero - My Smart Hero.\n\tsmartenemy - My Smart Enemy.\n\tsmartpowerup - My Smart Powerup.\n\tactor - Stays in the same place.\n\trandom - Moves randomly about the map avoiding recent places.\n\tkeyboard - Uses they keyboard to control.\n\tenemypursuer - Goes after the closest enemy.\n\theropursuer - Goes after the closest hero.\n\teatablepursuer - Goes after the closest eatable (or powerup).\n\tpoweruppursuer - Goes after the closest powerup.\n\tenemyavoider - Tries to go as far as possible from all enemies.\n\theroavoider - Tries to go as far as possible from all heroes.\n\teatableenemypa - Pursues eatables while avoiding enemies.\n\tpowerupenemypa - Pursues powerups while avoiding enemies.\n\tenemyheropa - Pursues enemies while avoiding heroes.\n");
+			printf(
+				"Options\n"
+				"\t--moves # – How many turns to allow before quitting.\n"
+				"\t--hero [id] – Use the actor with the given id for the heroes.\n"
+				"\t--enemy [id] – Use the actor with the given id for the enemies.\n"
+				"\t--eatable [id] – Use the actor with the given id for the eatables.\n"
+				"\t--powerup [id] – Use the actor with the given id for the powerups.\n"
+				"\t--delay-[type] # – Let actors of the given type only make moves every # turns.\n"
+				"\t--delay # – Make every move last # miliseconds.\n"
+				"\t--render-off – Do not render the map, or make timed delays. Useful if you want to use printf for debugging.\n"
+				"\t--window # gives a hero # moves to eat enemies after eating a powerup.\n"
+				"\n"
+				"Available id's\n"
+				"\tsimplehero - My Simple Hero.\n"
+				"\tsmarthero - My Smart Hero.\n"
+				"\tsmartenemy - My Smart Enemy.\n"
+				"\tsmartpowerup - My Smart Powerup.\n"
+				"\tactor - Stays in the same place.\n"
+				"\trandom - Moves randomly about the map avoiding recent places.\n"
+				"\tkeyboard - Uses they keyboard to control.\n"
+				"\tenemypursuer - Goes after the closest enemy.\n"
+				"\theropursuer - Goes after the closest hero.\n"
+				"\teatablepursuer - Goes after the closest eatable (or powerup).\n"
+				"\tpoweruppursuer - Goes after the closest powerup.\n"
+				"\tenemyavoider - Tries to go as far as possible from all enemies.\n"
+				"\theroavoider - Tries to go as far as possible from all heroes.\n"
+				"\teatableenemypa - Pursues eatables while avoiding enemies.\n"
+				"\tpowerupenemypa - Pursues powerups while avoiding enemies.\n"
+				"\tenemyheropa - Pursues enemies while avoiding heroes.\n"
+			);
 			return 0;
 		}
 	}
