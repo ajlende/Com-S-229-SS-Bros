3ccdfde7641f4b921574867a7cdc9553573935e1
Alex Lende <ajlende@iastate.edu>
2014-04-30 13:13:37 -0500

    asdf

--- SmartEnemy.cpp
+++ SmartEnemy.cpp
@@ -80,7 +80,7 @@ int SmartEnemy::selectNeighbor(GraphMap* map, int x, int y) {
 		return this->pursue(map, x, y);
 	case 2:
 		// printf("Selecting for lazyPursue\n");
-		return this->lazyPursue(map, x, y, 25);
+		return this->lazyPursue(map, x, y, 70);
 	case 3:
 		// printf("selecting for eatableGuard\n");
 		return this->eatableGuard(map, x, y);
