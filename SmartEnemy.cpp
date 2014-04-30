77f7ca39d20c197f80d3d19c21a38ca138c37452
Alex Lende <ajlende@iastate.edu>
2014-04-30 13:11:44 -0500

    less random

--- SmartEnemy.cpp
+++ SmartEnemy.cpp
@@ -80,7 +80,7 @@ int SmartEnemy::selectNeighbor(GraphMap* map, int x, int y) {
 		return this->pursue(map, x, y);
 	case 2:
 		// printf("Selecting for lazyPursue\n");
-		return this->lazyPursue(map, x, y, 50);
+		return this->lazyPursue(map, x, y, 25);
 	case 3:
 		// printf("selecting for eatableGuard\n");
 		return this->eatableGuard(map, x, y);
